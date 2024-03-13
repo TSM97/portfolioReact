40922eabe115683d0128eca558701b0333b73930
GiannisTsamadias <127104855+GiannisTsamadias@users.noreply.github.com>
2024-03-13 10:49:52 +0200

    Testing(motionValue framer motion hook)

--- src/Components/About/Components/MaskCursor.tsx
+++ src/Components/About/Components/MaskCursor.tsx
@@ -1,40 +1,40 @@
-import { backOut, motion } from "framer-motion";
-import useMousePosition from "../hooks/useMousePosition";
-import { useRef, useState } from "react";
-import "../mask.css";
+import { backOut, motion } from 'framer-motion';
+import useMousePosition from '../hooks/useMousePosition';
+import { useRef, useState } from 'react';
+import '../mask.css';
 
 export default function MaskCursor() {
   const [isHovered, setIsHovered] = useState(false);
   const [isInside, setIsInside] = useState(false);
   const elementRef = useRef(null);
-  const { x, y }: { x: number | null; y: number | null } = useMousePosition(
-    elementRef,
-    isInside
-  );
+  const { x, y } = useMousePosition(elementRef, isInside);
   const size = isHovered ? 350 : 30;
+
   return (
     <main
       ref={elementRef}
-      className="lg:text-5xl md:text-3xl sm:text-xl text-medium text-center"
+      className='lg:text-5xl md:text-3xl sm:text-xl text-medium text-center'
       onMouseMove={() => setIsInside(true)}
       onMouseLeave={() => setIsInside(false)}
     >
       <motion.div
-        className="mask flex items-center justify-center cursor-default w-full h-[100dvh] absolute"
+        className='mask flex items-center justify-center cursor-default w-full h-[100dvh] absolute'
         // Pt-[5dvh] for default font-family, in order to be aligned
         animate={{
-          WebkitMaskPosition: `${x && x - size / 2}px ${y && y - size / 2}px`,
+          WebkitMaskPosition: `${x && x.get() - size / 2}px ${
+            y && y.get() - size / 2
+          }px`,
           WebkitMaskSize: `${isInside ? size : 0}px`,
-          offset: ["end end", "start start"],
+          offset: ['end end', 'start start'],
         }}
         transition={{
-          type: "tween",
+          type: 'tween',
           ease: backOut,
           duration: 0.7,
         }}
       >
         <p
-          className="mx-[10dvw] w-3/4 text-"
+          className='mx-[10dvw] w-3/4 text-'
           onMouseEnter={() => {
             setIsHovered(true);
           }}
@@ -48,13 +48,13 @@ export default function MaskCursor() {
           Ofcourse, I always try to meet my friends
         </p>
       </motion.div>
-      <div className=" flex items-center justify-center cursor-default w-full h-[100dvh]">
-        <p className=" mx-[10dvw] w-3/4">
-          My <span className="text-[#e33d30]">passion for programming</span>{" "}
+      <div className=' flex items-center justify-center cursor-default w-full h-[100dvh]'>
+        <p className=' mx-[10dvw] w-3/4'>
+          My <span className='text-[#e33d30]'>passion for programming</span>{' '}
           extends far beyond the confines of the workweek. During my free time,
           you'll often find me diving deep into various coding projects,
-          exploring new technologies, and honing my skills in{" "}
-          <span className="text-[#e33d30]">FrontEnd development</span>.
+          exploring new technologies, and honing my skills in{' '}
+          <span className='text-[#e33d30]'>FrontEnd development</span>.
         </p>
       </div>
     </main>
--- src/Components/About/hooks/useMousePosition.tsx
+++ src/Components/About/hooks/useMousePosition.tsx
@@ -1,27 +1,36 @@
-import { useState, useEffect } from "react";
+import { useMotionValue, useSpring } from 'framer-motion';
+import { useState, useEffect } from 'react';
 
 export default function useMousePosition(
   elementRef: React.MutableRefObject<null>,
   isInside: boolean
 ) {
-  const [mousePosition, setMousePosition] = useState<{
-    x: number | null;
-    y: number | null;
-  }>({ x: null, y: null });
+  const mousePosition = {
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+  };
+
+  const { x, y } = mousePosition;
+
+  const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
+
+  const smoothMouse = {
+    x: useSpring(x, smoothOptions),
+
+    y: useSpring(y, smoothOptions),
+  };
 
   useEffect(() => {
     const updateMousePosition = (e: MouseEvent) => {
       if (isInside) {
         const { left, top } = elementRef.current.getBoundingClientRect();
-        setMousePosition({
-          x: e.clientX - left,
-          y: e.clientY - top,
-        });
+        x.set(e.clientX - left);
+        y.set(e.clientY - top);
       }
     };
-    window.addEventListener("mousemove", updateMousePosition);
-    return () => window.removeEventListener("mousemove", updateMousePosition);
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => window.removeEventListener('mousemove', updateMousePosition);
   }, [elementRef, isInside]);
 
-  return mousePosition;
+  return smoothMouse;
 }
