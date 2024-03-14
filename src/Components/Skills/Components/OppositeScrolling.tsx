3bfb19e0612fd71bf1c8de8682215cec2b7d09ef
TSM97 <Gtsamadias@hotmail.com>
2024-03-14 18:08:37 +0200

    Implementation ( Skills Component - Opposite scrolling )

--- src/Components/Skills/Components/OppositeScrolling.tsx
+++ src/Components/Skills/Components/OppositeScrolling.tsx
@@ -1,29 +1,31 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
-import { cards } from '../data/card';
-import SkillCard from './SkillCards';
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { cards } from "../data/card";
+import SkillCard from "./skillCards";
+
+// * Should try fix smoothness of scrollig - Correction
 
 const OppositeScrolling = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
-    offset: ['start start', 'end end'],
+    offset: ["start start", "end end"],
   });
-  const y = useTransform(scrollYProgress, [0, 1], ['-250vh', '250vh']);
+  const y = useTransform(scrollYProgress, [0, 1], ["-250vh", "250vh"]);
 
-  console.log('y: ' + y.get());
+  console.log("y: " + y.get());
 
   return (
-    <section ref={targetRef} className='h-[350dvh] bg-neutral-900'>
-      <section className='flex justify-center gap-x-2'>
+    <section ref={targetRef} className="h-[350dvh] bg-neutral-900">
+      <section className="flex justify-center gap-x-2">
         <div>
           {cards.map((card) => {
             return <SkillCard card={card} key={card.id} />;
           })}
         </div>
-        <div className='overflow-y-hidden w-[450px] sticky top-0'>
+        <div className="overflow-y-hidden w-[450px] sticky top-0">
           <motion.div
-            className='absolute h-screen left-0 right-0'
+            className="absolute h-screen left-0 right-0"
             style={{ top: y }}
           >
             {cards.map((card) => {
