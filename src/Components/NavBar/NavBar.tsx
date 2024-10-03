8d7ebab4b0d8d1181e8e116d4834bf3228f938b1
TSM97 <gtsamadias@hotmail.com>
2024-10-04 00:53:08 +0300

    Import( My New CV)

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,71 +4,71 @@ import {
   Navbar,
   NavbarContent,
   NavbarItem,
-} from '@nextui-org/react';
+} from "@nextui-org/react";
 
-import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
-import { handleEmailButtonClick } from './Utils';
-import useScreenSize from '../../hooks/useScreenSize';
-import LOGO from '../../svgs/LOGO';
+import { ThemeSwitcher } from "../ThemeSwitcher/ThemeSwitcher";
+import { handleEmailButtonClick } from "./Utils";
+import useScreenSize from "../../hooks/useScreenSize";
+import LOGO from "../../svgs/LOGO";
 export default function NavBar() {
   const { isTablet, isMobile } = useScreenSize();
   return (
     <>
-      <section className='grid grid-cols-2 lg:grid-cols-7 grid-rows-2 w-screen h-[15%] gap-x-unit-lg max-w-[100dvw]'>
-        <section className='flex text-red col-span-1 row-span-2 pt-7 place-self-center'>
+      <section className="grid grid-cols-2 lg:grid-cols-7 grid-rows-2 w-screen h-[15%] gap-x-unit-lg max-w-[100dvw]">
+        <section className="flex text-red col-span-1 row-span-2 pt-7 place-self-center">
           <LOGO
             className={`${
-              isMobile ? 'w-[120px] h-[80px]' : 'w-[150px] h-[120px]'
+              isMobile ? "w-[120px] h-[80px]" : "w-[150px] h-[120px]"
             }`}
           />
         </section>
 
         {isTablet ? (
-          <section className='text-center row-span-1 pt-3'>
+          <section className="text-center row-span-1 pt-3">
             <Button
-              color='primary'
-              variant='shadow'
-              size={`${isTablet ? 'sm' : 'lg'}`}
-              className='font-semibold mr-2'
+              color="primary"
+              variant="shadow"
+              size={`${isTablet ? "sm" : "lg"}`}
+              className="font-semibold mr-2"
               onClick={handleEmailButtonClick}
             >
               Hire me
             </Button>
             <Button
               as={Link}
-              href='/CV/CV.pdf'
+              href="/CV/MyResume.pdf"
               download
-              color='primary'
-              variant='bordered'
-              size={`${isTablet ? 'sm' : 'lg'}`}
-              className='font-semibold border-4 mt-2'
+              color="primary"
+              variant="bordered"
+              size={`${isTablet ? "sm" : "lg"}`}
+              className="font-semibold border-4 mt-2"
             >
               Download CV
             </Button>
           </section>
         ) : (
-          <Navbar maxWidth='2xl' isBordered className='col-span-6'>
-            <NavbarContent className='hidden sm:flex gap-4' justify='end'>
-              <NavbarItem className='text-lg' isActive>
+          <Navbar maxWidth="2xl" isBordered className="col-span-6">
+            <NavbarContent className="hidden sm:flex gap-4" justify="end">
+              <NavbarItem className="text-lg" isActive>
                 <Button
-                  color='primary'
-                  variant='shadow'
-                  size={`${isTablet ? 'sm' : 'lg'}`}
-                  className='font-semibold'
+                  color="primary"
+                  variant="shadow"
+                  size={`${isTablet ? "sm" : "lg"}`}
+                  className="font-semibold"
                   onClick={handleEmailButtonClick}
                 >
                   Hire me
                 </Button>
               </NavbarItem>
-              <NavbarItem className='text-lg' isActive>
+              <NavbarItem className="text-lg" isActive>
                 <Button
                   as={Link}
-                  href='/CV/CV.pdf'
+                  href="/CV/MyResume.pdf"
                   download
-                  color='primary'
-                  variant='bordered'
-                  size='lg'
-                  className='font-semibold border-4'
+                  color="primary"
+                  variant="bordered"
+                  size="lg"
+                  className="font-semibold border-4"
                 >
                   Download CV
                 </Button>
